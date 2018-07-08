5f75cbd0cf7c2722c7a00d38f0d006efa527eb03
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-08 13:22:50 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,2 +1,12 @@
 class CreateCostumeStores < ActiveRecord::Migration[4.2]
+
+  def change
+   create_table :costume_stores do |t|
+     t.string :name
+     t.integer :employee_count
+     t.string :location
+     t.boolean :in_business
+     t.string :costume_inventory
+   end
+ end
 end
