677f652ba5ea52e9e057bef0e5be6739281d0cd9
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-08 13:35:58 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -7,8 +7,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
      t.integer :price
      t.string :image_url
      t.string :size
-     t.datetime :created_at
-     t.datetime :updated_at
+     t.timestamps null: false
    end
  end
 end
