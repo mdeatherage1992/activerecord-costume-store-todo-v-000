c72ed0b5278c8b6682001ea43c1025a62167a4f0
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-08 13:17:47 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 3) do
 
-  create_table "costume", force: :cascade do |t|
+  create_table "costumes", force: :cascade do |t|
     t.string "name"
     t.integer "price"
     t.string "image_url"
